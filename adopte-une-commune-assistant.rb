fcc04a72054b863d6f29d208f0ac03b137d2f27c
Grégoire Seux <grego_github@familleseux.net>
2023-11-30 19:52:06 +0100

    🚧 Replicate clochers.org url construction on client-side
    This will be useful to also download the page to parse its content

--- adopte-une-commune-assistant.rb
+++ adopte-une-commune-assistant.rb
@@ -31,6 +31,31 @@ def kvize(hash, separator: '&')
   hash.map { |k, v| "#{k}=#{v}" }.join(separator)
 end
 
+def build_clochers_org_url(lat:, lon:)
+  # This method is doing an emulation of "https://bano.openstreetmap.fr/pifometre/clochers.html?lat=#{lat}&lon=#{lon}"
+  raise ArgumentError("lat #{lat} must be within (-90..90)") unless (-90..90).include?(lat.round(0))
+  raise ArgumentError("lon #{lat} must be within (-180..180)") unless (-180..180).include?(lon.round(0))
+
+  uri = URI.parse("https://bano.openstreetmap.fr/pifometre/insee_from_coords.py?lat=#{lat}&lon=#{lon}")
+  request = Net::HTTP::Get.new(uri)
+  req_options = {
+    use_ssl: uri.scheme == 'https'
+  }
+
+  response = Net::HTTP.start(uri.hostname, uri.port, req_options) do |http|
+    http.request(request)
+  end
+  raise "Invalid code when querying insee code: #{response.code}" unless response.code.to_i == 200
+
+  insee_code = JSON.parse(response.body)[0][0]
+  department = if insee_code =~ /^97/
+                 insee_code[0...3]
+               else
+                 insee_code[0...2]
+               end
+  "https://clochers.org/Fichiers_HTML/Accueil/Accueil_clochers/#{department}/accueil_#{insee_code}.htm"
+end
+
 get '/version' do
   uri = URI.parse("http://localhost:#{ENV['JOSM_CONTROL_PORT']}/version")
   r = proxy_request(uri)
@@ -42,7 +67,9 @@ get '/load_and_zoom' do
   lon = params['left'].to_f + (params['right'].to_f - params['left'].to_f) / 2
   lat = params['bottom'].to_f + (params['top'].to_f - params['bottom'].to_f) / 2
 
-  redirect_url = "https://bano.openstreetmap.fr/pifometre/clochers.html?lat=#{lat}&lon=#{lon}"
+  redirect_url = build_clochers_org_url(lat: lat, lon: lon)
+
+  puts "Opening #{redirect_url}"
   Mixlib::ShellOut.new("xdg-open '#{redirect_url}'").run_command.error!
 
   # prepare the request to proxy
